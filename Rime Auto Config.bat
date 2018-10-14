a75836ac081a33b714f4b0117ad13afb5f6f6109
dqwyy <dqwyy1997@gmail.com>
2018-10-14 11:58:36 +0800

    Update Rime Auto Config.bat

--- Rime Auto Config.bat
+++ Rime Auto Config.bat
@@ -4,6 +4,6 @@ copy /Y "bopomofo.custom.yaml" "%appdata%\Rime\"
 copy /Y "default.custom.yaml" "%appdata%\Rime\"
 copy /Y "symbols_dqwyy.schema.yaml" "%appdata%\Rime\"
 copy /Y "weasel.custom.yaml" "%appdata%\Rime\"
-copy /Y "symbols_dqwyy.dict.yaml" "C:\Program Files\Rime\weasel-0.11.0\data\"
-::start "" "C:\Program Files\Rime\weasel-0.11.0\WeaselDeployer.exe"
+copy /Y "symbols_dqwyy.dict.yaml" "C:\Program Files\Rime\weasel-0.11.1\data\"
+::start "" "C:\Program Files\Rime\weasel-0.11.1\WeaselDeployer.exe"
 pause
